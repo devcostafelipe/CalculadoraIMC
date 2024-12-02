e6528bb7eddc30157e040c6ce8c509cbb227fcf6
devcostafelipe <devcostafelipe@gmail.com>
2024-12-02 14:45:00 -0300

    Mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +19,34 @@ class MainActivity : AppCompatActivity() {
 
         Btncalcular.setOnClickListener {
 
-            val peso = edtpeso.text
-            val altura = edtaltura.text
-            println("Cacular " + peso)
+            val pesoStr: String = edtpeso.text.toString()
+            val alturaStr: String = edtaltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+                //mostrar mensagem para o usuário
+
+                Snackbar.make(
+                           edtpeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+
+
+
+                ).show()
+
+            } else {
+
+                val peso = edtpeso.text.toString().toFloat()
+                val altura = edtaltura.text.toString().toFloat()
+
+                val alturaq2 = altura * altura
+                val resultado = peso / alturaq2
+
+                println("Cacular " + resultado)
+
+            }
+
+
         }
-
-
-
     }
 }
